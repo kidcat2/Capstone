56765bf105df387346b9c187752d7afb460a5326
kidcat2 <jaemin2056@naver.com>
2024-11-07 16:23:05 +0900

    20241107test

---
+++ train.py
@@ -0,0 +1,11 @@
+import geopandas as gpd
+import pandas as pd
+import matplotlib.pyplot as plt
+from shapely.geometry import Polygon, LineString, Point
+import requests
+import os
+import glob
+import sys
+import numpy as np
+
+
